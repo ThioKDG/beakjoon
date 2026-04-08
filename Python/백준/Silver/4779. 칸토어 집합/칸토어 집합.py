75979d0497c7f77df96df9ee7c2726a45ef9614a
Kim Dong Geon <113421699+ThioKDG@users.noreply.github.com>
2026-04-08 10:44:24 +0900

    [Silver III] Title: 칸토어 집합, Time: 32 ms, Memory: 33796 KB -BaekjoonHub

---
+++ Python/백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -0,0 +1,19 @@
+import sys
+def getCantor(size):
+    # base condition
+    if size == 1: return '-'
+
+    # divide 
+    new_size = size // 3
+    center = ' ' * new_size
+    side = getCantor(new_size)
+    return side + center + side
+
+while True:
+    try:
+        N = int(input())
+        size = 3 ** N
+        result_str = getCantor(size)
+        print(result_str)
+    except EOFError:
+        break
